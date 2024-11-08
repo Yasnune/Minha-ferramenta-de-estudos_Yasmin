e94e4dab3c16e144a6bd273a37a41a7a5d8fd31a
Yasnune <0000110408241xsp@al.educacao.sp.gov.br>
2024-11-08 11:26:56 -0300

    Add files via upload

---
+++ Minha ferramenta de estudo_Yasmin/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é o Github?',
+    'O Github é um reposotorio de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é a bolsa de valores?',
+    'Espaço de negociação de açoes de empresas de capital aberto.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz arroz em Inglês?',
+    'Arroz em ingles é rice'
+)
